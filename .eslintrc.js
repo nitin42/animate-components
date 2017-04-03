46a5891a2b96c2cd18c85faba7d35d3bc7d8aef1
Nitin <tulswani19@gmail.com>
2017-04-04 00:09:48 +0530

    added new rules

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+    	"import/no-named-as-default": 0,
+    	"import/no-named-as-default-member": 0,
+    	"no-tabs": 0,
+    	"indent": 0
+    }
+};
