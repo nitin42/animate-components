885cb15f960479c23c2bec5485cef4f46a8fe299
Nitin <tulswani19@gmail.com>
2017-04-12 16:17:35 +0530

    umd build

--- index.js
+++ index.js
@@ -1,113 +1,117 @@
-'use strict';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports', './lib/animations/bounce/index', './lib/animations/fade/index', './lib/animations/flip/index', './lib/animations/lightspeed/index', './lib/animations/rotate/index', './lib/animations/special', './lib/animations/slide/index'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require('./lib/animations/bounce/index'), require('./lib/animations/fade/index'), require('./lib/animations/flip/index'), require('./lib/animations/lightspeed/index'), require('./lib/animations/rotate/index'), require('./lib/animations/special'), require('./lib/animations/slide/index'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.index, global.index, global.index, global.index, global.index, global.special, global.index);
+    global.main = mod.exports;
+  }
+})(this, function (exports, _index, _index3, _index5, _index7, _index9, _special, _index11) {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.Hatch = exports.Entrance = exports.ExpandUp = exports.SlideRight = exports.SlideLeft = exports.SlideDown = exports.SlideUp = exports.Pulse = exports.Hinge = exports.Zoom = exports.Swing = exports.Rubber = exports.RollIn = exports.RollOut = exports.Flash = exports.RotateUpLeft = exports.RotateUpRight = exports.RotateLeft = exports.RotateRight = exports.RotateIn = exports.LightIn = exports.LightOut = exports.FlipY = exports.FlipX = exports.Flip = exports.FadeInRightBig = exports.FadeInLeftBig = exports.FadeInUpBig = exports.FadeInDown = exports.FadeInLeft = exports.FadeInRight = exports.FadeInUp = exports.FadeIn = exports.BounceDown = exports.BounceLeft = exports.BounceRight = exports.BounceUp = exports.Bounce = undefined;
+
+  var _index2 = _interopRequireDefault(_index);
+
+  var _index4 = _interopRequireDefault(_index3);
+
+  var _index6 = _interopRequireDefault(_index5);
+
+  var _index8 = _interopRequireDefault(_index7);
+
+  var _index10 = _interopRequireDefault(_index9);
+
+  var _special2 = _interopRequireDefault(_special);
+
+  var _index12 = _interopRequireDefault(_index11);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  var Bounce = _index2.default.Bounce,
+      BounceUp = _index2.default.BounceUp,
+      BounceRight = _index2.default.BounceRight,
+      BounceLeft = _index2.default.BounceLeft,
+      BounceDown = _index2.default.BounceDown;
+  var FadeIn = _index4.default.FadeIn,
+      FadeInUp = _index4.default.FadeInUp,
+      FadeInRight = _index4.default.FadeInRight,
+      FadeInLeft = _index4.default.FadeInLeft,
+      FadeInDown = _index4.default.FadeInDown,
+      FadeInUpBig = _index4.default.FadeInUpBig,
+      FadeInRightBig = _index4.default.FadeInRightBig,
+      FadeInLeftBig = _index4.default.FadeInLeftBig;
+  var Flip = _index6.default.Flip,
+      FlipX = _index6.default.FlipX,
+      FlipY = _index6.default.FlipY;
+  var LightOut = _index8.default.LightOut,
+      LightIn = _index8.default.LightIn;
+  var RotateIn = _index10.default.RotateIn,
+      RotateRight = _index10.default.RotateRight,
+      RotateLeft = _index10.default.RotateLeft,
+      RotateUpRight = _index10.default.RotateUpRight,
+      RotateUpLeft = _index10.default.RotateUpLeft;
+  var Flash = _special2.default.Flash,
+      RollOut = _special2.default.RollOut,
+      RollIn = _special2.default.RollIn,
+      Rubber = _special2.default.Rubber,
+      Swing = _special2.default.Swing,
+      Zoom = _special2.default.Zoom,
+      Hinge = _special2.default.Hinge,
+      Pulse = _special2.default.Pulse,
+      ExpandUp = _special2.default.ExpandUp,
+      Entrance = _special2.default.Entrance,
+      Hatch = _special2.default.Hatch;
+  var SlideUp = _index12.default.SlideUp,
+      SlideRight = _index12.default.SlideRight,
+      SlideLeft = _index12.default.SlideLeft,
+      SlideDown = _index12.default.SlideDown;
+  exports.Bounce = Bounce;
+  exports.BounceUp = BounceUp;
+  exports.BounceRight = BounceRight;
+  exports.BounceLeft = BounceLeft;
+  exports.BounceDown = BounceDown;
+  exports.FadeIn = FadeIn;
+  exports.FadeInUp = FadeInUp;
+  exports.FadeInRight = FadeInRight;
+  exports.FadeInLeft = FadeInLeft;
+  exports.FadeInDown = FadeInDown;
+  exports.FadeInUpBig = FadeInUpBig;
+  exports.FadeInLeftBig = FadeInLeftBig;
+  exports.FadeInRightBig = FadeInRightBig;
+  exports.Flip = Flip;
+  exports.FlipX = FlipX;
+  exports.FlipY = FlipY;
+  exports.LightOut = LightOut;
+  exports.LightIn = LightIn;
+  exports.RotateIn = RotateIn;
+  exports.RotateRight = RotateRight;
+  exports.RotateLeft = RotateLeft;
+  exports.RotateUpRight = RotateUpRight;
+  exports.RotateUpLeft = RotateUpLeft;
+  exports.Flash = Flash;
+  exports.RollOut = RollOut;
+  exports.RollIn = RollIn;
+  exports.Rubber = Rubber;
+  exports.Swing = Swing;
+  exports.Zoom = Zoom;
+  exports.Hinge = Hinge;
+  exports.Pulse = Pulse;
+  exports.SlideUp = SlideUp;
+  exports.SlideDown = SlideDown;
+  exports.SlideLeft = SlideLeft;
+  exports.SlideRight = SlideRight;
+  exports.ExpandUp = ExpandUp;
+  exports.Entrance = Entrance;
+  exports.Hatch = Hatch;
 });
-exports.Hatch = exports.Entrance = exports.ExpandUp = exports.SlideRight = exports.SlideLeft = exports.SlideDown = exports.SlideUp = exports.Pulse = exports.Hinge = exports.Zoom = exports.Swing = exports.Rubber = exports.RollIn = exports.RollOut = exports.Flash = exports.RotateUpLeft = exports.RotateUpRight = exports.RotateLeft = exports.RotateRight = exports.RotateIn = exports.LightIn = exports.LightOut = exports.FlipY = exports.FlipX = exports.Flip = exports.FadeInRightBig = exports.FadeInLeftBig = exports.FadeInUpBig = exports.FadeInDown = exports.FadeInLeft = exports.FadeInRight = exports.FadeInUp = exports.FadeIn = exports.BounceDown = exports.BounceLeft = exports.BounceRight = exports.BounceUp = exports.Bounce = undefined;
-
-var _index = require('./lib/animations/bounce/index');
-
-var _index2 = _interopRequireDefault(_index);
-
-var _index3 = require('./lib/animations/fade/index');
-
-var _index4 = _interopRequireDefault(_index3);
-
-var _index5 = require('./lib/animations/flip/index');
-
-var _index6 = _interopRequireDefault(_index5);
-
-var _index7 = require('./lib/animations/lightspeed/index');
-
-var _index8 = _interopRequireDefault(_index7);
-
-var _index9 = require('./lib/animations/rotate/index');
-
-var _index10 = _interopRequireDefault(_index9);
-
-var _special = require('./lib/animations/special');
-
-var _special2 = _interopRequireDefault(_special);
-
-var _index11 = require('./lib/animations/slide/index');
-
-var _index12 = _interopRequireDefault(_index11);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var Bounce = _index2.default.Bounce,
-    BounceUp = _index2.default.BounceUp,
-    BounceRight = _index2.default.BounceRight,
-    BounceLeft = _index2.default.BounceLeft,
-    BounceDown = _index2.default.BounceDown;
-var FadeIn = _index4.default.FadeIn,
-    FadeInUp = _index4.default.FadeInUp,
-    FadeInRight = _index4.default.FadeInRight,
-    FadeInLeft = _index4.default.FadeInLeft,
-    FadeInDown = _index4.default.FadeInDown,
-    FadeInUpBig = _index4.default.FadeInUpBig,
-    FadeInRightBig = _index4.default.FadeInRightBig,
-    FadeInLeftBig = _index4.default.FadeInLeftBig;
-var Flip = _index6.default.Flip,
-    FlipX = _index6.default.FlipX,
-    FlipY = _index6.default.FlipY;
-var LightOut = _index8.default.LightOut,
-    LightIn = _index8.default.LightIn;
-var RotateIn = _index10.default.RotateIn,
-    RotateRight = _index10.default.RotateRight,
-    RotateLeft = _index10.default.RotateLeft,
-    RotateUpRight = _index10.default.RotateUpRight,
-    RotateUpLeft = _index10.default.RotateUpLeft;
-var Flash = _special2.default.Flash,
-    RollOut = _special2.default.RollOut,
-    RollIn = _special2.default.RollIn,
-    Rubber = _special2.default.Rubber,
-    Swing = _special2.default.Swing,
-    Zoom = _special2.default.Zoom,
-    Hinge = _special2.default.Hinge,
-    Pulse = _special2.default.Pulse,
-    ExpandUp = _special2.default.ExpandUp,
-    Entrance = _special2.default.Entrance,
-    Hatch = _special2.default.Hatch;
-var SlideUp = _index12.default.SlideUp,
-    SlideRight = _index12.default.SlideRight,
-    SlideLeft = _index12.default.SlideLeft,
-    SlideDown = _index12.default.SlideDown;
-exports.Bounce = Bounce;
-exports.BounceUp = BounceUp;
-exports.BounceRight = BounceRight;
-exports.BounceLeft = BounceLeft;
-exports.BounceDown = BounceDown;
-exports.FadeIn = FadeIn;
-exports.FadeInUp = FadeInUp;
-exports.FadeInRight = FadeInRight;
-exports.FadeInLeft = FadeInLeft;
-exports.FadeInDown = FadeInDown;
-exports.FadeInUpBig = FadeInUpBig;
-exports.FadeInLeftBig = FadeInLeftBig;
-exports.FadeInRightBig = FadeInRightBig;
-exports.Flip = Flip;
-exports.FlipX = FlipX;
-exports.FlipY = FlipY;
-exports.LightOut = LightOut;
-exports.LightIn = LightIn;
-exports.RotateIn = RotateIn;
-exports.RotateRight = RotateRight;
-exports.RotateLeft = RotateLeft;
-exports.RotateUpRight = RotateUpRight;
-exports.RotateUpLeft = RotateUpLeft;
-exports.Flash = Flash;
-exports.RollOut = RollOut;
-exports.RollIn = RollIn;
-exports.Rubber = Rubber;
-exports.Swing = Swing;
-exports.Zoom = Zoom;
-exports.Hinge = Hinge;
-exports.Pulse = Pulse;
-exports.SlideUp = SlideUp;
-exports.SlideDown = SlideDown;
-exports.SlideLeft = SlideLeft;
-exports.SlideRight = SlideRight;
-exports.ExpandUp = ExpandUp;
-exports.Entrance = Entrance;
-exports.Hatch = Hatch;
