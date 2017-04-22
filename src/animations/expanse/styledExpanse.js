5a55a5c9e5e8d634e13b675deffe2603f477eb95
Nitin <tulswani19@gmail.com>
2017-04-22 21:40:16 +0530

    added vendor prefixes

--- src/animations/expanse/styledExpanse.js
+++ src/animations/expanse/styledExpanse.js
@@ -3,13 +3,17 @@ import { keyframes } from 'styled-components';
 const expanseUp = keyframes`
 	0% {
 		opacity: 0;
+		-ms-transform-origin: 50% 0%;
 		transform-origin: 50% 0%;
+		-ms-transform: scale(.2) translate(0%, -200%);
 		transform: scale(.2) translate(0%, -200%);
 	}
 
 	100% {
 		opacity: 1;
+		-ms-transform-origin: 50% 0%;
 		transform-origin: 50% 0%;
+		-ms-transform: scale(1) translate(0%, 0%);
 		transform: scale(1) translate(0%, 0%);
 	}
 `;
@@ -17,13 +21,17 @@ const expanseUp = keyframes`
 const expanseDown = keyframes`
 	0% {
 		opacity: 0;
+		-ms-transform-origin: 50% 100%;
 		transform-origin: 50% 100%;
+		-ms-transform: scale(.2) translate(0%, 200%);
 		transform: scale(.2) translate(0%, 200%);
 	}
 
 	100% {
 		opacity: 1;
+		-ms-transform-origin: 50% 100%;
 		transform-origin: 50% 100%;
+		-ms-transform: scale(1) translate(0%, 0%);
 		transform: scale(1) translate(0%, 0%);
 	}
 `
@@ -31,13 +39,17 @@ const expanseDown = keyframes`
 const expanseLeft = keyframes`
 	0% {
 		opacity: 0;
+		-ms-transform-origin: 0% 50%;
 		transform-origin: 0% 50%;
+		-ms-transform: scale(.2) translate(-200%, 0%);
 		transform: scale(.2) translate(-200%, 0%);
 	}
 
 	100% {
 		opacity: 1;
+		-ms-transform-origin: 0% 50%;
 		transform-origin: 0% 50%;
+		-ms-transform: scale(1) translate(0%, 0%);
 		transform: scale(1) translate(0%, 0%);
 	}
 `;
@@ -45,13 +57,17 @@ const expanseLeft = keyframes`
 const expanseRight = keyframes`
 	0% {
 		opacity: 0;
+		-ms-transform-origin: 100% 50%;
 		transform-origin: 100% 50%;
+		-ms-transform: scale(.2) translate(200%, 0%);
 		transform: scale(.2) translate(200%, 0%);
 	}
 
 	100% {
 		opacity: 1;
+		-ms-transform-origin: 100% 50%;
 		transform-origin: 100% 50%;
+		-ms-transform: scale(1) translate(0%, 0%);
 		transform: scale(1) translate(0%, 0%);
 	}
 `;
