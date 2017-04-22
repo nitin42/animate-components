3047c1d6a69adb5f4921e927917e3d7cccd2a5ca
Nitin <tulswani19@gmail.com>
2017-04-22 21:40:38 +0530

    added vendor prefixes

--- src/animations/fade/styledFade.js
+++ src/animations/fade/styledFade.js
@@ -12,100 +12,105 @@ const fadeIn = keyframes`
 
 const left = keyframes`
   from {
-  	opacity: 0;
-  	-webkit-transform: translate3d(-100%, 0, 0);
+    opacity: 0;
+    transform: translate3d(-100%, 0, 0);
   }
 
   to {
-  	opacity: 1;
-  	-webkit-transform: none;
+    opacity: 1;
+    -ms-transform: none;
+    transform: none;
   }
 `;
 
 const leftBig = keyframes`
-	from {
-		opacity: 0;
-	 	-webkit-transform: translate3d(-2000px, 0, 0);
-	}
+  from {
+    opacity: 0;
+    transform: translate3d(-2000px, 0, 0);
+  }
 
-	to {
-		opacity: 1;
-		-webkit-transform: none;
-	}
-
+  to {
+    opacity: 1;
+    -ms-transform: none;
+    transform: none;
+  }
 `;
 
 const right = keyframes`
-	from {
-		opacity: 0;
-	 	-webkit-transform: translate3d(100%, 0, 0);
-	}
+  from {
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+  }
 
-	to {
-		opacity: 1;
-		-webkit-transform: none;
-	}
-
+  to {
+    opacity: 1;
+    -ms-transform: none;
+    transform: none;
+  }
 `;
 
 const rightBig = keyframes`
-	from {
-		opacity: 0;
-	 	-webkit-transform: translate3d(2000px, 0, 0);
-	}
+  from {
+    opacity: 0;
+    transform: translate3d(2000px, 0, 0);
+  }
 
-	to {
-		opacity: 1;
-		-webkit-transform: none;
-	}
-
+  to {
+    opacity: 1;
+    -ms-transform: none;
+    transform: none;
+  }
 `;
 
 const down = keyframes`
-	from {
-		opacity: 0;
-		-webkit-transform: translate3d(0, -100%, 0);
-	}
+  from {
+    opacity: 0;
+    transform: translate3d(0, -100%, 0);
+  }
 
-	to {
-		opacity: 1;
-		-webkit-transform: none;
-	}
+  to {
+    opacity: 1;
+    -ms-transform: none;
+    transform: none;
+  }
 `;
 
 const downBig = keyframes`
-	from {
-		opacity: 0;
-		-webkit-transform: translate3d(0, -2000px, 0);
-	}
+  from {
+    opacity: 0;
+    transform: translate3d(0, -2000px, 0);
+  }
 
-	to {
-		opacity: 1;
-		-webkit-transform: none;
-	}
+  to {
+    opacity: 1;
+    -ms-transform: none;
+    transform: none;
+  }
 `;
 
 const up = keyframes`
 	from {
 		opacity: 0;
-		-webkit-transform: translate3d(0, 100%, 0);
+		transform: translate3d(0, 100%, 0);
 	}
 
 	to {
 		opacity: 1;
-		-webkit-transform: none;
+    -ms-transform: none;
+		transform: none;
 	}
 `;
 
 const upBig = keyframes`
 	from {
 		opacity: 0;
-		-webkit-transform: translate3d(0, 2000px, 0);
+		transform: translate3d(0, 2000px, 0);
 	}
 
 	to {
 		opacity: 1;
-		-webkit-transform: none;
+    -ms-transform: none;
+		transform: none;
 	}
 `;
 
