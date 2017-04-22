c3fd727da315437fa6881e5280ec3a3d5f6b2dfa
Nitin <tulswani19@gmail.com>
2017-04-22 21:42:14 +0530

    added vendor prefixes

--- src/animations/perspective/styledPerspective.js
+++ src/animations/perspective/styledPerspective.js
@@ -2,11 +2,13 @@ import { keyframes } from 'styled-components';
 
 const perspectiveDown = keyframes`
   0% {
+    -ms-transform-origin: 0 100%;
     transform-origin: 0 100%;
     transform: perspective(800px) rotateX(0deg);
   }
 
   100% {
+    -ms-transform-origin: 0 100%;
     transform-origin: 0 100%;
     transform: perspective(800px) rotateX(-180deg);
   }
@@ -14,38 +16,44 @@ const perspectiveDown = keyframes`
 
 const perspectiveUp = keyframes`
   0% {
+    -ms-transform-origin: 0 0;
     transform-origin: 0 0;
     transform: perspective(800px) rotateX(0deg);
   }
 
   100% {
+    -ms-transform-origin: 0 0;
     transform-origin: 0 0;
     transform: perspective(800px) rotateX(180deg);
   }
 `
 
 const perspectiveRight = keyframes`
-	0% {
-		transform-origin: 100% 0;
-		transform: perspective(800px) rotateY(0deg);
-	}
+  0% {
+    -ms-transform-origin: 100% 0;
+    transform-origin: 100% 0;
+    transform: perspective(800px) rotateY(0deg);
+  }
 
-	100% {
-		transform-origin: 100% 0;
-		transform: perspective(800px) rotateY(180deg);
-	}
+  100% {
+    -ms-transform-origin: 100% 0;
+    transform-origin: 100% 0;
+    transform: perspective(800px) rotateY(180deg);
+  }
 `
 
 const perspectiveLeft = keyframes`
-	0% {
-		transform-origin: 0 0;
-		transform: perspective(800px) rotateY(0deg);
-	}
+  0% {
+    -ms-transform-origin: 0 0;
+    transform-origin: 0 0;
+    transform: perspective(800px) rotateY(0deg);
+  }
 
-	100% {
-		transform-origin: 0 0;
-		transform: perspective(800px) rotateY(-180deg);
-	}
+  100% {
+    -ms-transform-origin: 0 0;
+    transform-origin: 0 0;
+    transform: perspective(800px) rotateY(-180deg);
+  }
 `
 
 export {
