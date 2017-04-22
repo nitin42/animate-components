83813de999274168fa4ceda37fa384d532a41374
Nitin <tulswani19@gmail.com>
2017-04-22 21:42:35 +0530

    added vendor prefixes

--- src/animations/rotate/styledRotate.js
+++ src/animations/rotate/styledRotate.js
@@ -2,12 +2,14 @@ import { keyframes } from 'styled-components';
 
 const rotateIn = keyframes`
 	from {
+		-ms-transform-origin: center;
 		transform-origin: center;
 		transform: rotate3d(0, 0, 1, -200deg);
-		opacity: 0; 
+		opacity: 0;
 	}
 
 	to {
+		-ms-transform: none;
 		transform: none;
 		opacity: 1;
 	}
@@ -15,13 +17,16 @@ const rotateIn = keyframes`
 
 const rotateLeft = keyframes`
 	from {
+		-ms-transform-origin: left bottom;
 		transform-origin: left bottom;
 		transform: rotate3d(0, 0, 1, -45deg);
 		opacity: 0;
 	}
 
 	to {
+		-ms-transform-origin: left bottom;
 		transform-origin: left bottom;
+		-ms-transform: none;
 		transform: none;
 		opacity: 1;
 	}
@@ -29,13 +34,16 @@ const rotateLeft = keyframes`
 
 const rotateRight = keyframes`
 	from {
+		-ms-transform-origin: right bottom;
 		transform-origin: right bottom;
 		transform: rotate3d(0, 0, 1, 45deg);
 		opacity: 0;
 	}
 
 	to {
+		-ms-transform-origin: right bottom;
 		transform-origin: right bottom;
+		-ms-transform: none;
 		transform: none;
 		opacity: 1;
 	}
@@ -43,13 +51,16 @@ const rotateRight = keyframes`
 
 const rotateUpLeft = keyframes`
 	from {
+		-ms-transform-origin: left bottom;
 		transform-origin: left bottom;
 		transform: rotate3d(0, 0, 1, 45deg);
 		opacity: 0;
 	}
 
 	to {
+		-ms-transform-origin: left bottom;
 		transform-origin: left bottom;
+		-ms-transform: none;
 		transform: none;
 		opacity: 1;
 	}
@@ -57,13 +68,16 @@ const rotateUpLeft = keyframes`
 
 const rotateUpRight = keyframes`
 	from {
+		-ms-transform-origin: right bottom;
 		transform-origin: right bottom;
 		transform: rotate3d(0, 0, 1, -45deg);
 		opacity: 0;
 	}
 
 	to {
+		-ms-transform-origin: right bottom;
 		transform-origin: right bottom;
+		-ms-transform: none;
 		transform: none;
 		opacity: 1;
 	}
