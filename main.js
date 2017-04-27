9a4e95ac59e94a9a10d8c1b6e5a0c5f70b17ff64
Nitin <tulswani19@gmail.com>
2017-04-27 19:24:54 +0530

    exported keyframe rules

--- main.js
+++ main.js
@@ -3,16 +3,83 @@
  */
 
 // Animation Component.
-import bounceObj from './dist/animations/bounce/index';
-import fadeObj from './dist/animations/fade/index';
-import flipObj from './dist/animations/flip/index';
-import lightObj from './dist/animations/lightspeed/index';
-import rotateObj from './dist/animations/rotate/index';
-import specialObj from './dist/animations/special';
-import slideObj from './dist/animations/slide/index';
-import PObj from './dist/animations/perspective/index';
-import bingoObj from './dist/animations/Bingo/index';
-import expanseObj from './dist/animations/expanse/index';
+import bounceObj from "./dist/animations/bounce/index";
+import fadeObj from "./dist/animations/fade/index";
+import flipObj from "./dist/animations/flip/index";
+import lightObj from "./dist/animations/lightspeed/index";
+import rotateObj from "./dist/animations/rotate/index";
+import specialObj from "./dist/animations/special";
+import slideObj from "./dist/animations/slide/index";
+import PObj from "./dist/animations/perspective/index";
+import bingoObj from "./dist/animations/Bingo/index";
+import expanseObj from "./dist/animations/expanse/index";
+
+// Keyframes rules for <Merge />
+import {
+  bounce,
+  bounceDown,
+  bounceLeft,
+  bounceRight,
+  bounceUp
+} from "./dist/animations/bounce/styledBounce";
+import {
+  fadeIn,
+  left,
+  down,
+  downBig,
+  leftBig,
+  rightBig,
+  right,
+  up,
+  upBig
+} from "./dist/animations/fade/styledFade";
+import { flip, flipX, flipY } from "./dist/animations/flip/styledFlip";
+import { lightIn, lightOut } from "./dist/animations/lightspeed/styledLight";
+import {
+  rotateIn,
+  rotateLeft,
+  rotateRight,
+  rotateUpLeft,
+  rotateUpRight
+} from "./dist/animations/rotate/styledRotate";
+import {
+  flash,
+  rollOut,
+  rollIn,
+  rubber,
+  swing,
+  zoom,
+  hinge,
+  pulse,
+  expandUp,
+  entrance,
+  hatch,
+  starWars
+} from "./dist/animations/styledSpecial";
+import {
+  slideDown,
+  slideLeft,
+  slideRight,
+  slideUp
+} from "./dist/animations/slide/styledSlide";
+import {
+  perspectiveDown,
+  perspectiveUp,
+  perspectiveLeft,
+  perspectiveRight
+} from "./dist/animations/perspective/styledPerspective";
+import {
+  puffmein,
+  puffmeout,
+  vanishout,
+  vanishin
+} from "./dist/animations/Bingo/styledBingo";
+import {
+  expanseDown,
+  expanseUp,
+  expanseLeft,
+  expanseRight
+} from "./dist/animations/expanse/styledExpanse";
 
 const { Bounce, BounceUp, BounceRight, BounceLeft, BounceDown } = bounceObj;
 
@@ -24,7 +91,7 @@ const {
   FadeInDown,
   FadeInUpBig,
   FadeInRightBig,
-  FadeInLeftBig,
+  FadeInLeftBig
 } = fadeObj;
 
 const { Flip, FlipX, FlipY } = flipObj;
@@ -36,7 +103,7 @@ const {
   RotateRight,
   RotateLeft,
   RotateUpRight,
-  RotateUpLeft,
+  RotateUpLeft
 } = rotateObj;
 
 const {
@@ -114,5 +181,57 @@ export {
   ExpanseLeft,
   ExpanseRight,
   ExpanseDown,
-  ExpanseUp
+  ExpanseUp,
+  bounce,
+  bounceDown,
+  bounceLeft,
+  bounceRight,
+  bounceUp,
+  fadeIn,
+  left,
+  down,
+  downBig,
+  leftBig,
+  rightBig,
+  right,
+  up,
+  upBig,
+  flip,
+  flipX,
+  flipY,
+  lightIn,
+  lightOut,
+  rotateIn,
+  rotateLeft,
+  rotateRight,
+  rotateUpLeft,
+  rotateUpRight,
+  flash,
+  rollOut,
+  rollIn,
+  rubber,
+  swing,
+  zoom,
+  hinge,
+  pulse,
+  expandUp,
+  entrance,
+  hatch,
+  starWars,
+  slideDown,
+  slideLeft,
+  slideRight,
+  slideUp,
+  perspectiveDown,
+  perspectiveUp,
+  perspectiveLeft,
+  perspectiveRight,
+  puffmein,
+  puffmeout,
+  vanishout,
+  vanishin,
+  expanseDown,
+  expanseUp,
+  expanseLeft,
+  expanseRight
 };
