e2299f26f38d7fd43e45a33878b8e31d04392ccc
Nitin <tulswani19@gmail.com>
2017-05-21 16:54:12 +0530

    refactored

---
+++ src/mods/avoidNesting.js
@@ -0,0 +1,23 @@
+/*
+  Avoid DOM nesting of elements
+*/
+
+const avoidNest = (elementAs, reactChildren) => {
+  // No children to render. DOM nesting validated.
+  let elementType = elementAs;
+  if (!reactChildren) return elementType;
+
+  if (elementType === 'div') return elementType;
+
+  const { type } = reactChildren;
+
+  if (type === elementType) {
+    // Change to 'div' to validate DOM nesting
+    elementType = 'div';
+    // Should log the warning for DOM nesting validation ?
+  }
+
+  return elementType;
+};
+
+export default avoidNest;
--- src/mods/getElementType.js
+++ src/mods/getElementType.js
@@ -1,25 +1,16 @@
 /**
-  Computes or return default element type for the animation component.
-*/
-let computeElementType = props => {
-  const { as } = props;
-
-  return as === null ? "div" : as;
-};
-
-/**
   Returns the element type for the Component
 */
-let getElementType = (Component, props) => {
+const getElementType = (Component, props) => {
   const { defaultProps = {} } = Component;
 
   if (props.as && props.as !== defaultProps.as) return props.as;
 
-  const computedElement = computeElementType(props);
-  return computedElement ? computedElement : null;
-
-  // Redundant ?
-  return defaultProps.as || "div";
+  // const computedElement = computeElementType(props);
+  // return computedElement ? computedElement : null;
+  //
+  // // Redundant ?
+  return defaultProps.as || 'div';
 };
 
 export default getElementType;
