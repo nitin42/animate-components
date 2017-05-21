9da613928dc72e0f70c9d8d2f47dfd7189cb04e7
Nitin <tulswani19@gmail.com>
2017-05-21 16:55:54 +0530

    new example

--- starter/App.js
+++ starter/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 
-import Merge from "../src/animations/merge";
-
-import { left, bounce } from "../main";
-import { FadeIn, RotateIn } from "../main";
+import { FadeIn, Merge, left, rotateIn, RotateIn } from "../index";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <FadeIn duration="8s" as="h1">
-          Hello
+        <FadeIn
+          duration="7s"
+          as="h1"
+        >
+          Bye Bye React !
         </FadeIn>
+        <br/>
+        <FadeIn
+          duration="3s"
+          as="h2"
+          forceInterpolate={{ steps: 4, direction: "start" }}
+        >
+          Hello Angular!
+        </FadeIn>
+        <Merge
+          one={{ name: left, duration: "4s", timingFunction: "ease-in" }}
+          two={{ name: rotateIn, duration: "6s", timingFunction: "ease-out" }}
+          as="h1"
+        >
+          <h1>Hello World!</h1>
+        </Merge>
       </div>
     );
   }
