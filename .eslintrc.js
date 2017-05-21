aaed87335aa953ef50ad5a100ebe571d46c0b169
Nitin <tulswani19@gmail.com>
2017-05-21 14:27:05 +0530

    some changes

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": [
+      "airbnb",
+      "plugin:flowtype/recommended"
+    ],
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import",
+        "flowtype"
+    ],
+    "rules": {
+      "no-console": 0,
+      "consistent-return": 0,
+      "react/no-unused-prop-types": 0,
+      "react/require-default-props": 0,
+      "no-shadow": 0
+    }
+};
