bb8b69988dcbc9c1e2de908066e693713519a2fa
Nitin <tulswani19@gmail.com>
2017-06-01 01:37:28 +0530

    recheck

--- starter/App.js
+++ starter/App.js
@@ -9,21 +9,24 @@ class App extends React.Component {
     return (
       <div>
         <Merge
-          one={{ name: fadeIn, duration: '4', timingFunction: 'ease-in' }}
-          two={{ name: slideDown, duration: '2s', timingFunction: 'ease-out'}}
+          one={{ name: fadeIn, duration: "3s", timingFunction: "ease-in" }}
+          two={{ name: slideDown, duration: "6s", timingFunction: "ease-out" }}
           as="h1"
-          style={{ color: 'red' }}
+          style={{ color: "red" }}
         >
           Hello World
         </Merge>
-        <br/>
-        <FadeIn duration="4s" as="h1" style={{ color: 'red' }} timingFunction="linear" direction="reverse">
+        <br />
+        <FadeIn
+          duration="2s"
+          as="strong"
+          timingFunction="linear"
+          direction="reverse"
+        >
           Hello World
         </FadeIn>
-        <br/>
-        <ExpanseUp
-          duration="4s"
-        >
+        <br />
+        <ExpanseUp duration="4s">
           World Hello
         </ExpanseUp>
       </div>
