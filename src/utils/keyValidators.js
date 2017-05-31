ad77967214e414975880dae3702bb1696cdaac39
Nitin <tulswani19@gmail.com>
2017-06-01 01:36:48 +0530

    added eslint comments

--- src/utils/keyValidators.js
+++ src/utils/keyValidators.js
@@ -3,6 +3,7 @@
 // animation-name
 const names = (key, propValue) => {
   if (key === 'name' && typeof propValue[key] !== 'string') {
+    /* eslint-disable no-console */
     console.error('Warning: Failed propType. Prop value for animation name should be a string.');
   }
 };
@@ -10,11 +11,14 @@ const names = (key, propValue) => {
 // animation-duration
 const duration = (key, propValue) => {
   if (key === 'duration' && typeof propValue[key] !== 'string') {
+    /* eslint-disable no-console */
     console.error('Warning: Failed propType. Prop value for animation duration should be a string. For eg - \'2s\'');
   }
 };
 
 // animation-timing-function
+
+/* eslint-disable consistent-return */
 const timingFunction = (key, propValue) => {
   if (key === 'timingFunction' && typeof propValue[key] === 'string') {
     const arr = [
@@ -33,6 +37,7 @@ const timingFunction = (key, propValue) => {
       "step-start", "step-end"]. Note steps(#, start|end) is currently not supported.
     `;
 
+    /* eslint-disable no-console */
     return arr.includes(propValue[key]) ? null : console.error(err);
   } else if (key === 'timingFunction' && typeof propValue[key] !== 'string') {
     console.error('Warning: Failed propType. Prop value for tf should be a string. For eg - \'ease-out\'');
@@ -43,6 +48,7 @@ const timingFunction = (key, propValue) => {
 
 const direction = (key, propValue) => {
   if (key === 'direction' && typeof propValue[key] !== 'string') {
+    /* eslint-disable no-console */
     console.error('Warning: Failed propType. Prop value for "direction" should be a string.');
   }
 };
@@ -51,6 +57,7 @@ const interpolateValidators = (key) => {
   const keys = ['steps', 'direction'];
   const err = `Warning: Unknown prop '${key}' passed to the Merge component.`;
 
+  /* eslint-disable no-console */
   return keys.includes(key) ? null : console.error(err);
 };
 
@@ -60,6 +67,7 @@ const propValidators = (key) => {
   const keys = ['name', 'duration', 'timingFunction'];
   const err = `Warning: Unknown prop '${key}' passed to the Merge component. Prop should be one of [${keys}]`;
 
+  /* eslint-disable no-console */
   return keys.includes(key) ? null : console.error(err);
 };
 
