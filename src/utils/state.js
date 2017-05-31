29e96a26790fa4ccdbfe202a061bdc1d46bdc928
Nitin <tulswani19@gmail.com>
2017-05-31 12:40:33 +0530

    removed a comment

--- src/utils/state.js
+++ src/utils/state.js
@@ -12,7 +12,6 @@ const derive = (props, AnimationName) => {
     forceInterpolate,
   } = props;
 
-  // Normal animate components is not working due to this logic. Refactor it ...
   if (forceInterpolate) {
     return `${AnimationName} ${duration} steps(${forceInterpolate.steps}, ${forceInterpolate.direction}) ${delay} ${iterations} ${fillMode} ${playState}`;
   }
