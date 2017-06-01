21608637e461848cf6807854f690319739570a30
Nitin <tulswani19@gmail.com>
2017-06-02 01:21:51 +0530

    new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
       "object-shorthand": 0,
       "func-names": 0,
       "no-restricted-syntax": 0,
-      "no-unused-expressions": 0
+      "no-unused-expressions": 0,
+      "quotes": 0,
     }
 };
