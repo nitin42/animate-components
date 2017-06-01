c24ac6033f7e9fe8b394828535e22f901bb3494c
Nitin <tulswani19@gmail.com>
2017-06-02 01:19:58 +0530

    added new example

---
+++ examples/componentProp.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import { LightOut } from "../index";
+import Comp from './comp';
+import "./index.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Using `component` prop</h1>
+        <LightOut
+          duration="4s"
+          as="h2"
+          component={Comp}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
