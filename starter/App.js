103702d8bc08207451df59f99f62830085dd20bb
Nitin <tulswani19@gmail.com>
2017-06-04 15:36:51 +0530

    check example

--- starter/App.js
+++ starter/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { fadeIn, slideDown, Merge, FadeIn, ExpanseUp, PuffIn, LightOut } from "../dist/animate-components.min.js";
+import { fadeIn, slideDown, Merge, FadeIn, ExpanseUp, PuffIn, Delay, LightOut } from "../dist/animate-components.min.js";
 
 import Comp from './comp';
 
@@ -10,45 +10,31 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Using component prop</h1>
-        <Merge
-          one={{ name: fadeIn, duration: "3s", timingFunction: "ease-in" }}
-          two={{ name: slideDown, duration: "6s", timingFunction: "ease-out" }}
-          component={Comp}
-          className="main"
-        />
-        <br />
-        <LightOut
-          duration="4s"
-          as="h2"
-          component={Comp}
-        />
-        <br />
-        <ExpanseUp duration="4s" component={Comp} />
-        <br/>
-        <h1>Using components</h1>
-        <Merge
-          one={{ name: fadeIn, duration: "3s", timingFunction: "ease-in" }}
-          two={{ name: slideDown, duration: "6s", timingFunction: "ease-out" }}
-          className="main"
-        >
-          <Comp />
-        </Merge>
-        <br />
-        <LightOut
-          duration="4s"
-          as="h2"
-        >
-          <Comp />
-        </LightOut>
-        <br />
-        <ExpanseUp duration="4s">
-          <Comp />
-        </ExpanseUp>
-        <PuffIn duration="3s" as="h1" component={Comp} />
+        <Delay timeout={4000}>
+          <FadeIn duration="3s" as="h1" component={Comp} />
+        </Delay>
       </div>
     );
   }
 }
 
 export default App;
+
+/**
+
+New feature
+
+- Delay rendering of components along with the animation Because it seems weird when component
+has been rendered by React but animations are delayed. This will keep both in sync and will be
+processed through the timeout prop (type: Number). Though this won't be replacing the delay
+attribute of an animation but its an addon for existing library.
+
+- timeout prop
+=> set this prop after the component has been mounted with a boolean state variable
+=> Implement this feature for only high order component container's components (Animation Component)
+=> Rendering process for the component class will change
+
+
+
+
+*/
