191f3ecb9d0b42e6938f5c198915eb70a18e5500
Nitin <tulswani19@gmail.com>
2017-06-09 12:44:41 +0530

    init

--- starter/App.js
+++ starter/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { fadeIn, slideDown, Merge, FadeIn, ExpanseUp, PuffIn, Delay, LightOut } from "../dist/animate-components.min.js";
+import { Flash } from "../dist/animate-components.min.js";
 
 import Comp from './comp';
 
@@ -10,31 +10,12 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Delay timeout={4000}>
-          <FadeIn duration="3s" as="h1" component={Comp} />
-        </Delay>
+        <Flash duration="3s" as="h1" iterations="infinite" style={{ color: '#ff8080' }}>
+          <h1>Animate Components</h1>
+        </Flash>
       </div>
     );
   }
 }
 
-export default App;
-
-/**
-
-New feature
-
-- Delay rendering of components along with the animation Because it seems weird when component
-has been rendered by React but animations are delayed. This will keep both in sync and will be
-processed through the timeout prop (type: Number). Though this won't be replacing the delay
-attribute of an animation but its an addon for existing library.
-
-- timeout prop
-=> set this prop after the component has been mounted with a boolean state variable
-=> Implement this feature for only high order component container's components (Animation Component)
-=> Rendering process for the component class will change
-
-
-
-
-*/
+export default App; 
