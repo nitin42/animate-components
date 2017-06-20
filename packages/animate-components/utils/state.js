95345bff34fae53d121695d60c0e5c717e52ec88
Nitin <tulswani19@gmail.com>
2017-06-20 15:17:34 +0530

    updated

---
+++ packages/animate-components/utils/state.js
@@ -0,0 +1,22 @@
+// Utilities for changing the component state based on a particular prop.
+
+const derive = (props, AnimationName) => {
+  const {
+    duration,
+    timingFunction,
+    delay,
+    direction,
+    iterations,
+    fillMode,
+    playState,
+    forceInterpolate,
+  } = props;
+
+  if (forceInterpolate) {
+    return `${AnimationName} ${duration} steps(${forceInterpolate.steps}, ${forceInterpolate.direction}) ${delay} ${iterations} ${fillMode} ${playState}`;
+  }
+
+  return `${AnimationName} ${duration} ${timingFunction} ${delay} ${iterations} ${direction} ${fillMode} ${playState}`;
+};
+
+export default derive;
