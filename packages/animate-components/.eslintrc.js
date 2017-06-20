c0ba63bf34a32000a31d119e7b4ed4529cd97d76
Nitin <tulswani19@gmail.com>
2017-06-20 15:19:00 +0530

    disable consistent-return

---
+++ packages/animate-components/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+      "import/no-named-as-default": 0,
+      "import/no-named-as-default-member": 0,
+      "react/jsx-filename-extension": 0,
+      "react/prop-types": 0,
+      "consistent-return": 0,
+    }
+};
