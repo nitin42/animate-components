ec12cb4d832ca2304ac7eb5db3095f1e0c587d05
Nitin <tulswani19@gmail.com>
2017-06-23 09:35:32 +0530

    relative import for hoc

---
+++ packages/animate-keyframes/src/keyframes.js
@@ -0,0 +1,112 @@
+import {
+  Bouncing,
+  Fade,
+  Flippy,
+  Light,
+  Rotate,
+  Special,
+  Slide,
+  Perspective,
+  Bingo,
+  Expanse
+} from "./main";
+
+const { bounce, bounceDown, bounceUp, bounceLeft, bounceRight } = Bouncing;
+const {
+  fadeIn,
+  left,
+  leftBig,
+  right,
+  rightBig,
+  down,
+  downBig,
+  up,
+  upBig
+} = Fade;
+const { flip, flipX, flipY } = Flippy;
+const { lightIn, lightOut } = Light;
+const {
+  rotateIn,
+  rotateRight,
+  rotateLeft,
+  rotateUpLeft,
+  rotateUpRight
+} = Rotate;
+const {
+  flash,
+  rollOut,
+  rollIn,
+  rubber,
+  swing,
+  zoom,
+  hinge,
+  pulse,
+  expandUp,
+  entrance,
+  hatch,
+  starWars
+} = Special;
+const { slideUp, slideDown, slideLeft, slideRight } = Slide;
+const {
+  perspectiveDown,
+  perspectiveUp,
+  perspectiveRight,
+  perspectiveLeft
+} = Perspective;
+const { puffmeout, puffmein, vanishin, vanishout } = Bingo;
+const { expanseUp, expanseRight, expanseDown, expanseLeft } = Expanse;
+
+export {
+  bounce,
+  bounceDown,
+  bounceLeft,
+  bounceRight,
+  bounceUp,
+  fadeIn,
+  left,
+  down,
+  downBig,
+  leftBig,
+  rightBig,
+  right,
+  up,
+  upBig,
+  flip,
+  flipX,
+  flipY,
+  lightIn,
+  lightOut,
+  rotateIn,
+  rotateLeft,
+  rotateRight,
+  rotateUpLeft,
+  rotateUpRight,
+  flash,
+  rollOut,
+  rollIn,
+  rubber,
+  swing,
+  zoom,
+  hinge,
+  pulse,
+  expandUp,
+  entrance,
+  hatch,
+  starWars,
+  slideDown,
+  slideLeft,
+  slideRight,
+  slideUp,
+  perspectiveDown,
+  perspectiveUp,
+  perspectiveLeft,
+  perspectiveRight,
+  puffmein,
+  puffmeout,
+  vanishout,
+  vanishin,
+  expanseDown,
+  expanseUp,
+  expanseLeft,
+  expanseRight
+};
